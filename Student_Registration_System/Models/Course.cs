cf7adeaae734397236a12decff431bbfe3398a1c
sanybhat <sanybhat@USBLRSANYBHAT1>
2023-04-03 10:49:53 +0530

    Stage 1

--- Student_Registration_System/Models/Course.cs
+++ Student_Registration_System/Models/Course.cs
@@ -7,7 +7,6 @@ namespace Student_Registration_System.Models
         [Key]
         public int CourseID { get; set; }
         [Required]
-        [StringLength(50)]
         public string CourseName { get;set; }
     }
 }
